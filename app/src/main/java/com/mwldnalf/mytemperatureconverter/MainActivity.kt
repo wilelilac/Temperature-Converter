c9304645b8fe07d688fa6f5788eae778df2e1871
wilelilac <wildfahreza@gmail.com>
2023-11-14 09:28:56 +0700

    stateless converter

--- app/src/main/java/com/mwldnalf/mytemperatureconverter/MainActivity.kt
+++ app/src/main/java/com/mwldnalf/mytemperatureconverter/MainActivity.kt
@@ -34,7 +34,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    StatefulTemperatureInput()
+                    Column {
+                        StatefulTemperatureInput()
+                        ConverterApp()
+                    }
                 }
             }
         }
@@ -70,6 +73,47 @@ private fun convertToFahrenheit(celcius: String) =
     celcius.toDoubleOrNull()?.let {
         (it * 9 / 5) + 32
     }.toString()
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun StatelessTemperatureInput(
+    input: String,
+    output: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+){
+    Column(modifier.padding(16.dp)) {
+        Text(
+            text = stringResource(R.string.stateless_converter),
+            style = MaterialTheme.typography.headlineSmall
+        )
+        OutlinedTextField(
+            value = input,
+            label = { Text(stringResource(R.string.enter_celsius)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = onValueChange,
+            )
+        Text(stringResource(R.string.temperature_fahrenheit, output))
+    }
+}
+
+@Composable
+private fun ConverterApp(
+    modifier: Modifier = Modifier,
+) {
+    var input by remember { mutableStateOf("") }
+    var output by remember { mutableStateOf("") }
+    Column(modifier) {
+        StatelessTemperatureInput(
+            input = input,
+            output = output,
+            onValueChange = { newInput ->
+                input = newInput
+                output = convertToFahrenheit(newInput)
+            }
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
